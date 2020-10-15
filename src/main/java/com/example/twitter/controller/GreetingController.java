b4f99604c8f471609801b870dbb98ff49b7218dd
vadymnastoiashchyi <vadim.89@ukr.net>
2020-10-15 12:21:38 +0300

    Initial commit

--- src/main/java/com/example/twitter/controller/GreetingController.java
+++ src/main/java/com/example/twitter/controller/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -35,6 +36,18 @@ public class GreetingController {
         Message message = new Message(text, tag);
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
         model.put("messages", messages);
         return "main";
     }
--- src/main/java/com/example/twitter/repository/MessageRepository.java
+++ src/main/java/com/example/twitter/repository/MessageRepository.java
@@ -3,5 +3,8 @@ package com.example.twitter.repository;
 import com.example.twitter.model.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
